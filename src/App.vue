ebda21b1af18f9efa72bcacacd2b7ae80231b6f9
LupoArticoblu <wolf94-@libero.it>
2024-07-08 12:12:50 +0200

    App r&m [impostazioni generali]

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Rick and Morty App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+ @use './styles/General.scss';
+</style>
