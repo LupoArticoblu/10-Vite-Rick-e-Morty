d21c7eac7f93e0c25b27d3323b7d6323500c4dcf
LupoArticoblu <wolf94-@libero.it>
2024-07-12 17:58:12 +0200

    computed

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <p class="text-center py-3">Personaggi visualizzati in pagina: {{ store.characters.length }}</p>
+    <p class="text-center py-3"> {{ outputResults }}</p>
   </div>
 </template>
 
@@ -11,6 +11,16 @@ export default {
   data() {
     return {
       store
+    }
+  },
+  //LE COMPUTED sono metodi che vengono richiamati solo e soltanto quando un dato reattivo all'interno di esse cambia, senza la necessita di ricaricare la pagina e tutti i suoi contenuti
+  computed: {
+    outputResults() {
+      if (store.characters.length > 0) {
+        return 'Personaggi trovati: ' + store.characters.length;
+      }
+      //il return è obbligatorio
+      return 'Nessun personaggio trovato';
     }
   }
 }
